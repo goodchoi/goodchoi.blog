e16482ad24fbd1d0dd3c9033e2d6c938f631d769
goodchoi <jangu3384@gmail.com>
2024-03-19 21:34:42 +0900

    사이트 도메인 변경

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "goodchoi blog",
   description: "심연 끝에 성장하는 개발자",
   author: "Goodchoi",
-  siteUrl: "https://goochoiblog.netlify.app/",
+  siteUrl: "https://goodchoi.site",
   links: {
     github: "https://github.com/goodchoi",
     email: "mailto:jangu3384@gmail.com",
