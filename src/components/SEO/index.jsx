b6a464fab979ea22260f6f7ea1ce9722251dd320
goodchoi <jangu3384@gmail.com>
2024-03-30 16:26:49 +0900

    google search console verify

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -5,6 +5,7 @@ import { siteUrl } from "../../../blog-config"
 const SEO = ({ title, description, url }) => {
   return (
     <Helmet>
+      <meta name="google-site-verification" content="dPP7oQgTqsUd91x5vnPkK3lmX8M5cXXTF3ZAMKPHbrQ" />
       <title>{title}</title>
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
