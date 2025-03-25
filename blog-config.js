e5f906f9f1307123e24543a5738e12a9f0b4beae
goodchoi <jangu3384@gmail.com>
2025-03-25 17:09:03 +0900

    도메인 변경

--- blog-config.js
+++ blog-config.js
@@ -2,11 +2,12 @@ module.exports = {
   title: "goodchoi's blog",
   description: "차분히 또 차분히 🚶🏻",
   author: "Goodchoi",
-  siteUrl: "https://goodchoi.site",
+  siteUrl: "https://goodchoi.blog",
   links: {
     github: "https://github.com/goodchoi",
     email: "mailto:jangu3384@gmail.com",
   },
+  useAbout: true,
   utterances: {
     repo: "goodchoi/goodchoi.blog",
     type: "pathname",
